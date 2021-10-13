86466fb4d655e960e73d509e5243d8b066e95694
ari <mariia.barabukha@ukma.edu.ua>
2021-10-13 13:14:07 +0300

    change annotation for api in rest controller

--- src/main/java/sb/com/project/controllers/GroupController.java
+++ src/main/java/sb/com/project/controllers/GroupController.java
@@ -1,11 +1,8 @@
 package sb.com.project.controllers;
 
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import sb.com.project.BL.StGroup;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import sb.com.project.services.GroupService;
 
 import javax.swing.*;
@@ -16,9 +13,14 @@ public class GroupController {
     @Autowired
     GroupService groupService;
 
-    @RequestMapping(value = "/getAllGroups", method = RequestMethod.GET)
-    public List<StGroup> getAllGroups(){
-        return  groupService.readGroups();
+//    @RequestMapping(value = "/getAllGroups", method = RequestMethod.GET)
+//    public List<StGroup> getAllGroups(){
+//        return  groupService.readGroups();
+//    }
+
+    @GetMapping("/groups")
+    List<StGroup> all() {
+        return groupService.readGroups();
     }
 
     @RequestMapping(value = "createGroup", method = RequestMethod.POST)
@@ -31,7 +33,7 @@ public class GroupController {
         return groupService.updateGroup(group);
     }
 
-    @RequestMapping(value = "deleteGroup", method = RequestMethod.DELETE)
+    @RequestMapping(value = "deleteGroup/{id}", method = RequestMethod.DELETE)
     public String deleteStudent(@RequestBody StGroup group){
 
         return groupService.deleteGroup(group);
